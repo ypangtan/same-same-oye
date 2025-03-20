0df409c8a0b0c158dc8c827d7633f2b432caf775
OrenTwh <orenwh12@gmail.com>
2025-03-20 16:26:14 +0800

    test

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -60,7 +60,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(100);
+            return Limit::perMinute(200);
         });
     }
 }
