c454ff888246b685c68d4f9afbaa6c0c2ab9c484
YanPang <nicholastanyanpang@gmail.com>
2025-09-02 14:27:19 +0800

    update

--- resources/views/admin/lucky_draw_reward/edit.blade.php
+++ resources/views/admin/lucky_draw_reward/edit.blade.php
@@ -5,7 +5,7 @@ $lucky_draw_reward_edit = 'lucky_draw_reward_edit';
 <div class="nk-block-head nk-block-head-sm">
     <div class="nk-block-between">
         <div class="nk-block-head-content">
-            <h3 class="nk-block-title page-title">{{ __( 'template.add_x', [ 'title' => Str::singular( __( 'template.lucky_draw_rewards' ) ) ] ) }}</h3>
+            <h3 class="nk-block-title page-title">{{ __( 'template.edit_x', [ 'title' => Str::singular( __( 'template.lucky_draw_rewards' ) ) ] ) }}</h3>
         </div><!-- .nk-block-head-content -->
     </div><!-- .nk-block-between -->
 </div><!-- .nk-block-head -->
