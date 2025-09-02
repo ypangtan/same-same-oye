08a574c239ecc2ec9ea92610b663aa50fdc470c3
YanPang <nicholastanyanpang@gmail.com>
2025-09-02 14:43:10 +0800

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -133,7 +133,7 @@ class User extends Model
     
         $rank = Rank::where( 'status', 10 )
             ->orderBy('priority', 'ASC')
-            ->first();
+            ->get();
 
         $data = [];
         
