c54863d5ae55e11db411e237d55b8d494e727e21
YanPang <nicholastanyanpang@gmail.com>
2025-09-23 01:21:13 +0800

    update

--- resources/views/admin/script.blade.php
+++ resources/views/admin/script.blade.php
@@ -139,7 +139,7 @@
                 menus.push( dropdownMenu );
 
                 // detach it and append it to the body
-                $( 'body' ).append( dropdownMenu.detach() );
+                $( 'dt-container' ).append( dropdownMenu.detach() );
 
                 // grab the new offset position
                 let eOffset = target.offset();
