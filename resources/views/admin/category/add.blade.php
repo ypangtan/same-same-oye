094ba369b6b56e39b440160825d96830e66bcefa
YanPang <nicholastanyanpang@gmail.com>
2025-12-29 00:42:43 +0800

    update

--- resources/views/admin/category/add.blade.php
+++ resources/views/admin/category/add.blade.php
@@ -49,7 +49,7 @@ $parent_route = $data['parent_route'] ?? null;
 <div class="nk-block-head nk-block-head-sm">
     <div class="nk-block-between">
         <div class="nk-block-head-content">
-            <h3 class="nk-block-title page-title">{{ __( 'template.add_x', [ 'title' => Str::singular( __( 'template.categorys' ) ) ] ) }}</h3>
+            <h3 class="nk-block-title page-title">{{ __( 'template.add_x', [ 'title' => Str::singular( __( 'template.categories' ) ) ] ) }}</h3>
         </div><!-- .nk-block-head-content -->
     </div><!-- .nk-block-between -->
 </div><!-- .nk-block-head -->
