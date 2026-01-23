7044044749230c64e8b6e52f680fe12a945766ca
YanPang <nicholastanyanpang@gmail.com>
2026-01-23 19:35:39 +0800

    up

--- resources/views/admin/playlist/index.blade.php
+++ resources/views/admin/playlist/index.blade.php
@@ -170,7 +170,7 @@ var statusMapper = @json( $data['status'] ),
                 targets: parseInt( '{{ Helper::columnIndex( $columns, "image" ) }}' ),
                 
                 render: function( data, type, row, meta ) {
-                    return data ? `<image src='${data}'>` : '-' ;
+                    return data ? `<image src='${data}' width="75px">` : '-' ;
                 },
             },
             {
