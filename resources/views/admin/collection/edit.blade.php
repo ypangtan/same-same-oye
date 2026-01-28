5dda4bf79d773f09429995f4fdb94b226c0c24fd
YanPang <nicholastanyanpang@gmail.com>
2026-01-28 16:53:11 +0800

    up

--- resources/views/admin/collection/edit.blade.php
+++ resources/views/admin/collection/edit.blade.php
@@ -56,7 +56,8 @@ $parent_route = $data['parent_route'] ?? '';
                     <div class="col-sm-7">
                         <div class="form-check form-switch">
                             <select class="form-select" id="{{ $collection_edit }}_display_type">
-                                @foreach( $data['display_types'] as $type )
+                                @foreach( $data['display_types'] as $key => $type )
+                                {{ dd( $type ); }}
                                 <option value="{{ $type['value'] }}">{{ $type['title'] }}</option>
                                 @endforeach
                             </select>
