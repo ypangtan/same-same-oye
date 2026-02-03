89bcd480c0040a9bf072340d1883fc795ec1ce49
YanPang <nicholastanyanpang@gmail.com>
2026-02-03 20:15:11 +0800

    up

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -26,8 +26,6 @@ class PaymentService {
             $plan = SubscriptionPlan::findOrFail($data['plan_id']);
             $jws  = $data['receipt_data'];
 
-            return $jws;
-
             if (!self::isJWS($jws)) {
                 throw new Exception('Invalid receipt format: not a StoreKit 2 JWS');
             }
