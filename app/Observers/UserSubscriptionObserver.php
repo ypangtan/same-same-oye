15df50b0737797527a5012f68cab317670f777bc
YanPang <nicholastanyanpang@gmail.com>
2026-02-23 16:42:04 +0800

    update

--- app/Observers/UserSubscriptionObserver.php
+++ app/Observers/UserSubscriptionObserver.php
@@ -11,32 +11,32 @@ class UserSubscriptionObserver
         $user = User::find( $userSubscription->user_id );
         $user->checkPlanValidity();
 
-        $members = $userSubscription->member()->get();
-        foreach( $members as $member ) {
-            $user = User::find( $member->user_id );
-            $user->checkPlanValidity();
-        }
+        // $members = $userSubscription->member()->get();
+        // foreach( $members as $member ) {
+        //     $user = User::find( $member->user_id );
+        //     $user->checkPlanValidity();
+        // }
     }
 
     public function updated( UserSubscription $userSubscription ) {
         $user = User::find( $userSubscription->user_id );
         $user->checkPlanValidity();
 
-        $members = $userSubscription->member()->get();
-        foreach( $members as $member ) {
-            $user = User::find( $member->user_id );
-            $user->checkPlanValidity();
-        }
+        // $members = $userSubscription->member()->get();
+        // foreach( $members as $member ) {
+        //     $user = User::find( $member->user_id );
+        //     $user->checkPlanValidity();
+        // }
     }
 
     public function deleted( UserSubscription $userSubscription) {
         $user = User::find( $userSubscription->user_id );
         $user->checkPlanValidity();
 
-        $members = $userSubscription->member()->get();
-        foreach( $members as $member ) {
-            $user = User::find( $member->user_id );
-            $user->checkPlanValidity();
-        }
+        // $members = $userSubscription->member()->get();
+        // foreach( $members as $member ) {
+        //     $user = User::find( $member->user_id );
+        //     $user->checkPlanValidity();
+        // }
     }
 }
