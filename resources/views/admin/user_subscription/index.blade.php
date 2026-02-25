8d2870c66f971e73b1492ec5ed4c06d9ce485d9b
YanPang <nicholastanyanpang@gmail.com>
2026-02-25 13:27:36 +0800

    update add

--- resources/views/admin/user_subscription/index.blade.php
+++ resources/views/admin/user_subscription/index.blade.php
@@ -3,6 +3,20 @@
         <div class="nk-block-head-content">
             <h3 class="nk-block-title page-title">{{ __( 'template.user_subscriptions' ) }}</h3>
         </div><!-- .nk-block-head-content -->
+        @can( 'add user_subscriptions' )
+        <div class="nk-block-head-content">
+            <div class="toggle-wrap nk-block-tools-toggle">
+                <a href="#" class="btn btn-icon btn-trigger toggle-expand me-n1" data-target="pageMenu"><em class="icon ni ni-more-v"></em></a>
+                <div class="toggle-expand-content" data-content="pageMenu">
+                    <ul class="nk-block-tools g-3">
+                        <li class="nk-block-tools-opt">
+                            <a href="{{ route( 'admin.user_subscription.add' ) }}" class="btn btn-primary">{{ __( 'template.add' ) }}</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div><!-- .nk-block-head-content -->
+        @endcan
     </div><!-- .nk-block-between -->
 </div><!-- .nk-block-head -->
 
