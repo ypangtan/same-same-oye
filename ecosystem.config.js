d09174d371b341abedaab7f8185f15a24a760650
YanPang <nicholastanyanpang@gmail.com>
2026-03-12 18:00:20 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,8 +9,8 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: "200M",
-      error_file: "storage/logs/pm2-worker-error.log",
-      out_file: "storage/logs/pm2-worker-out.log",
+      error_file: "pm2/logs/pm2-worker-error.log",
+      out_file: "pm2/logs/pm2-worker-out.log",
     }
   ]
 }
