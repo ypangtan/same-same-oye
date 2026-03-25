e65e1c8d421c5c3afdf791e6ec0ff8e34ebef429
YanPang <nicholastanyanpang@gmail.com>
2026-03-25 10:48:16 +0800

    update observer

--- app/Observers/SubscriptionGroupMemberObserver.php
+++ app/Observers/SubscriptionGroupMemberObserver.php
@@ -9,21 +9,26 @@ use App\Models\SubscriptionGroupMember;
 class SubscriptionGroupMemberObserver {
 
     public function created( SubscriptionGroupMember $member ) {
-        CheckUserPlanValidityJob::dispatch($member->user_id);
+        \DB::afterCommit( function() use ( $member ) {
+            CheckUserPlanValidityJob::dispatch($member->user_id);
+        } );
     }
 
     public function updated( SubscriptionGroupMember $member ) {
-        CheckUserPlanValidityJob::dispatch($member->user_id);
+        \DB::afterCommit( function() use ( $member ) {
+            CheckUserPlanValidityJob::dispatch($member->user_id);
 
-        // 处理旧用户
-        if ( $member->wasChanged('user_id') ) {
-            $oldUserId = $member->getOriginal('user_id');
-            CheckUserPlanValidityJob::dispatch($oldUserId);
-        }
+            // 处理旧用户
+            if ( $member->wasChanged('user_id') ) {
+                $oldUserId = $member->getOriginal('user_id');
+                CheckUserPlanValidityJob::dispatch($oldUserId);
+            }
+        } );
     }
 
     public function deleted( SubscriptionGroupMember $member ) {
-        CheckUserPlanValidityJob::dispatch($member->user_id);
-        
+        \DB::afterCommit( function() use ( $member ) {
+            CheckUserPlanValidityJob::dispatch($member->user_id);
+        } );
     }
 }
--- app/Observers/UserSubscriptionObserver.php
+++ app/Observers/UserSubscriptionObserver.php
@@ -10,39 +10,38 @@ use App\Models\UserSubscription;
 class UserSubscriptionObserver {
 
     public function created( UserSubscription $userSubscription ) {
-        CheckUserPlanValidityJob::dispatch( $userSubscription->user_id );
+        \DB::afterCommit( function() use ( $userSubscription ) {
+            CheckUserPlanValidityJob::dispatch( $userSubscription->user_id );
 
-        $userSubscription->member()
-            ->pluck('user_id')
-            ->each( fn($userId) => CheckUserPlanValidityJob::dispatch( $userId ) );
+            $userSubscription->member()
+                ->pluck('user_id')
+                ->each( fn($userId) => CheckUserPlanValidityJob::dispatch( $userId ) );
 
-        $userSubscription->group()->each( fn($member) => $member->delete() );
+            $userSubscription->group()->get()->each( fn($member) => $member->delete() );
+        } );
     }
 
     public function updated( UserSubscription $userSubscription ) {
-        CheckUserPlanValidityJob::dispatch( $userSubscription->user_id );
+        \DB::afterCommit( function() use ( $userSubscription ) {
+            CheckUserPlanValidityJob::dispatch( $userSubscription->user_id );
 
-        // If the subscription is not active, we need to remove all the member of the plan.
-        $members = SubscriptionGroupMember::where( 'leader_id', $userSubscription->user_id )->get();
+            // If the subscription is not active, we need to remove all the member of the plan.
+            $members = SubscriptionGroupMember::where( 'leader_id', $userSubscription->user_id )->get();
 
-        if( $userSubscription->status != 10 ) {
-            try {
-                \DB::beginTransaction();
-                foreach( $members as $member ) {
-                    $member->delete();
+            if( $userSubscription->status != 10 ) {
+                try {
+                    foreach( $members as $member ) {
+                        $member->delete();
+                    }
+                } catch (\Throwable $e) {
+                    \Log::error('Failed to remove subscription members: ' . $e->getMessage());
+                    throw $e;
                 }
-                \DB::commit();
-            } catch (\Throwable $e) {
-                \DB::rollBack();
-                \Log::error('Failed to remove subscription members: ' . $e->getMessage());
-                throw $e;
+            } else {
+                foreach( $members as $member ) {
+                    CheckUserPlanValidityJob::dispatch( $member->user_id );
+                }
             }
-        } else {
-            foreach( $members as $member ) {
-                CheckUserPlanValidityJob::dispatch( $member->id );
-            }
-        }
-
-
+        } );
     }
 }
