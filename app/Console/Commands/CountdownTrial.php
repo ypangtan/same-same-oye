d18408e3a52f2f75e174072f357179045cf73895
YanPang <nicholastanyanpang@gmail.com>
2026-04-03 14:46:48 +0800

    update countdown notification lang

--- app/Console/Commands/CountdownTrial.php
+++ app/Console/Commands/CountdownTrial.php
@@ -42,8 +42,8 @@ class CountdownTrial extends Command
     {
 
         $now = Carbon::now()->timezone( 'Asia/Kuala_Lumpur' );
-        $day1 = $now->copy()->subDay();
-        $day3 = $now->copy()->subDays(3);
+        $day1 = $now->copy()->addDay();
+        $day3 = $now->copy()->addDays(3);
 
         $notifyUserSubscription = UserSubscription::where( 'type', 2 )
             ->where( 'status', 10 )
