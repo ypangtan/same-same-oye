176a6c525f820880d5875ee0861ce15b731baf23
YanPang <nicholastanyanpang@gmail.com>
2026-06-22 02:49:37 +0800

    update

--- app/Http/Controllers/Api/StreamController.php
+++ app/Http/Controllers/Api/StreamController.php
@@ -15,12 +15,7 @@ class StreamController extends Controller
      * Record an activity (stream, banner click, or popup click).
      *
      * @group Activity API
-     *
-     * <strong>type</strong><br>
-     * stream: Record a stream event<br>
-     * banner_click: Record a banner click<br>
-     * popup_click: Record a popup click<br>
-     * <br>
+     * 
      * <strong>content_type</strong> (required when type=stream)<br>
      * 1: Radio<br>
      * 2: Item<br>
@@ -29,7 +24,6 @@ class StreamController extends Controller
      * 5: Banner<br>
      * 6: Pop Announcement<br>
      *
-     * @bodyParam type string required One of: stream, banner_click, popup_click. Example: stream
      * @bodyParam content_type integer Required when type=stream. One of: 1, 2, 3, 4, 5, 6. Example: 2
      * @bodyParam radio_name string nullable. Example: "Hits FM"
      * @bodyParam item_id integer Required when content_type=2. Example: 5
