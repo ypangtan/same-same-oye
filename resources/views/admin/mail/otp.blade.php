b9ad19af5f32390928d1c7871390b5476f7ac023
YanPang <nicholastanyanpang@gmail.com>
2026-06-23 18:56:09 +0800

    up

--- resources/views/admin/mail/otp.blade.php
+++ resources/views/admin/mail/otp.blade.php
@@ -55,24 +55,26 @@
         <img src="{{ asset('admin/images/logo.png') }}" alt="Sama Sama Oye! Logo" class="logo">
     </div>
 
-    <p>Hello,</p>
-    <p>Thank you for signing up with Sama Sama Oye!</p>
-    <p>Your OTP code is:</p>
-    <div class="otp">{{ $data['otp_code'] }}</div>
-    <p>This code will expire in 10 minutes.</p>
-    <p>For your security:</p>
+    <div class="otp-container">
+        <p>Hello,</p>
+        <p>Thank you for signing up with Sama Sama Oye!</p>
+        <p>Your OTP code is:</p>
+        <div class="otp">{{ $data['otp_code'] }}</div>
+        <p>This code will expire in 10 minutes.</p>
+        <p>For your security:</p>
 
-    <ul>
-        <li>Do not share this code with anyone.</li>
-        <li>Sama Sama Oye! staff will never ask for your verification code.</li>
-        <li>If you did not request for this code, please ignore this email.</li>
-    </ul>
+        <ul>
+            <li>Do not share this code with anyone.</li>
+            <li>Sama Sama Oye! staff will never ask for your verification code.</li>
+            <li>If you did not request for this code, please ignore this email.</li>
+        </ul>
 
-    <p>Need help? Contact our support team at <a href="mailto:contact@samasamaoye.com">contact@samasamaoye.com</a>.</p>
+        <p>Need help? Contact our support team at <a href="mailto:contact@samasamaoye.com">contact@samasamaoye.com</a>.</p>
 
-    <p>Thank you,<br>
-    Sama Sama Oye! Support Team</p>
-    <p><a href="https://www.samasamaoye.com/en">https://www.samasamaoye.com/en</a></p>
+        <p>Thank you,<br>
+        Sama Sama Oye! Support Team</p>
+        <p><a href="https://www.samasamaoye.com/en">https://www.samasamaoye.com/en</a></p>
+    </div>
 
     <div class="footer">
         <p class="text-center">This is an automated message. Please do not reply to this email.</p>
