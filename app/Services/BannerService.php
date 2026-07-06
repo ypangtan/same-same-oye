50398dee9eea34c62b02edf3990b3138dd1e31d6
YanPang <nicholastanyanpang@gmail.com>
2026-07-06 10:57:05 +0800

    update

--- app/Services/BannerService.php
+++ app/Services/BannerService.php
@@ -58,7 +58,7 @@ class BannerService
                 'url' => '',
                 'sequence' => 1,
                 'status' => 10,
-                'priority' => 0,
+                'priority' => max( Banner::max( 'priority' ) + 1, 1 ),
             ]);
 
             // $name = $request->file( 'file' )->getClientOriginalName();
