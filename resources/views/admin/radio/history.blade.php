840c677f3057d78759543ef500db320a8821029a
YanPang <nicholastanyanpang@gmail.com>
2026-09-14 16:18:57 +0800

    up

--- resources/views/admin/radio/history.blade.php
+++ resources/views/admin/radio/history.blade.php
@@ -122,7 +122,7 @@ document.addEventListener( 'DOMContentLoaded', function() {
     </div>
 </div>
 
-<div class="d-flex flex-wrap gap-3 mb-4">
+<div class="d-flex flex-wrap mb-4" style="gap:1rem;">
     <input type="text" class="form-control form-control-sm" style="max-width:220px;" id="listener_ip_search" placeholder="{{ __( 'datatables.search_x', [ 'title' => __( 'radio.listener_ip' ) ] ) }}" />
     <input type="text" class="form-control form-control-sm" style="max-width:220px;background-color:#fff;" id="listener_start_search" placeholder="{{ __( 'datatables.search_x', [ 'title' => __( 'radio.listener_start' ) ] ) }}" />
     <input type="text" class="form-control form-control-sm" style="max-width:220px;background-color:#fff;" id="listener_end_search" placeholder="{{ __( 'datatables.search_x', [ 'title' => __( 'radio.listener_end' ) ] ) }}" />
