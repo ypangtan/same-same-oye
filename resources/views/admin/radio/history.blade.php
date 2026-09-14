232ef44ed8baefa1ce76256953ee38d2d9480476
YanPang <nicholastanyanpang@gmail.com>
2026-09-14 16:13:41 +0800

    up

--- resources/views/admin/radio/history.blade.php
+++ resources/views/admin/radio/history.blade.php
@@ -121,9 +121,9 @@ document.addEventListener( 'DOMContentLoaded', function() {
 </div>
 
 <div class="listing-filter">
-    <input type="text" class="form-control form-control-sm" style="max-width:220px;" id="listener_ip_search" placeholder="{{ __( 'datatables.search_x', [ 'title' => __( 'radio.listener_ip' ) ] ) }}" />
-    <input type="text" class="form-control form-control-sm" style="max-width:220px;background-color:#fff;" id="listener_start_search" placeholder="{{ __( 'datatables.search_x', [ 'title' => __( 'radio.listener_start' ) ] ) }}" />
-    <input type="text" class="form-control form-control-sm" style="max-width:220px;background-color:#fff;" id="listener_end_search" placeholder="{{ __( 'datatables.search_x', [ 'title' => __( 'radio.listener_end' ) ] ) }}" />
+    <input type="text" class="form-control form-control-sm" id="listener_ip_search" placeholder="{{ __( 'datatables.search_x', [ 'title' => __( 'radio.listener_ip' ) ] ) }}" />
+    <input type="text" class="form-control form-control-sm" style="background-color:#fff;" id="listener_start_search" placeholder="{{ __( 'datatables.search_x', [ 'title' => __( 'radio.listener_start' ) ] ) }}" />
+    <input type="text" class="form-control form-control-sm" style="background-color:#fff;" id="listener_end_search" placeholder="{{ __( 'datatables.search_x', [ 'title' => __( 'radio.listener_end' ) ] ) }}" />
 </div>
 
 <div class="card card-bordered card-preview">
